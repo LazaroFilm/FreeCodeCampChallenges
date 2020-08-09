e028578e5e372cead774ef711a24acf21e44e00f
Victor Lazaro <lazarofilm@gmail.com>
2020-08-09 14:58:11 -0400

    commit

--- JavaScript/JavaScript Algorithms and Data Structures Projects/JavaScript Algorithms and Data Structures Projects- Cash Register.js
+++ JavaScript/JavaScript Algorithms and Data Structures Projects/JavaScript Algorithms and Data Structures Projects- Cash Register.js
@@ -1,9 +1,9 @@
 function checkCashRegister(price, cash, cid) {
   var change;
-  return change;
+  return "hello world!";
 }
 
-checkCashRegister(19.5, 20, [
+console.log(checkCashRegister(19.5, 20, [
   ["PENNY", 1.01],
   ["NICKEL", 2.05],
   ["DIME", 3.1],
@@ -13,4 +13,11 @@ checkCashRegister(19.5, 20, [
   ["TEN", 20],
   ["TWENTY", 60],
   ["ONE HUNDRED", 100],
-]);
+]));
+
+//take price - cash = due
+//then look into cid (cash in drawer) and see how to return the change due. 
+//start with the biggest bills and go down. 
+//if we run out of large bills, go to smaller ones
+//at the end check how much left in cid. if we have the exact change or more -> status closed
+//if we are too short to return the change -> status open 
--- JavaScript/JavaScript Algorithms and Data Structures Projects/JavaScript Algorithms and Data Structures Projects- Roman Numeral Converter.js
+++ JavaScript/JavaScript Algorithms and Data Structures Projects/JavaScript Algorithms and Data Structures Projects- Roman Numeral Converter.js
@@ -1,16 +1,31 @@
 function convertToRoman(num) {
-  const digit = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-  const roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
-  let answer = ""
+  const digit = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const roman = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I",
+  ];
+  let answer = "";
 
   //checking digit/roman equivalence
   // for (let i = 0; i < digit.length; i++) {
   //   console.log(digit[i] + " = " + roman[i]);
   // }
+  
   for (let i = 0; i < digit.length; i++) {
     while (num >= digit[i]) {
-      answer += roman[i]
-      num -= digit[i]
+      answer += roman[i];
+      num -= digit[i];
     }
   }
   return answer;
