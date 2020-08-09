86137f29362fa9c654d9b96750afcc80f43be5ad
Victor Lazaro <lazarofilm@gmail.com>
2020-08-09 14:25:00 -0400

    commit

--- JavaScript/JavaScript Algorithms and Data Structures Projects- Cash Register.js
+++ JavaScript/JavaScript Algorithms and Data Structures Projects- Cash Register.js
@@ -3,4 +3,14 @@ function checkCashRegister(price, cash, cid) {
   return change;
 }
 
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+checkCashRegister(19.5, 20, [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100],
+]);
