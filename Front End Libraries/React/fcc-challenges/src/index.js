939eb7d98e57f5b484670ddb53a712590a188238
Victor Lazaro <lazarofilm@gmail.com>
2020-08-18 16:14:08 -0400

    commit

--- Front End Libraries/React/fcc-challenges/src/index.js
+++ Front End Libraries/React/fcc-challenges/src/index.js
@@ -1,16 +1,5 @@
-import React from 'react';
+import React, { createElement } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-var element = React.createElement('h1', {
-  className: 'greeting'
-}, 'Hello, world!');
-ReactDOM.render(element, document.getElementById('root'));
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+const element = <h1>Hello World</h1>;
+ReactDOM.render(element, document.getElementById('root'))
