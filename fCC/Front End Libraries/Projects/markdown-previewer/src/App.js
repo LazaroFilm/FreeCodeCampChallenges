d06b9f2868b6e39f44daa9714f62e868f5f5d76c
Victor Lazaro <lazarofilm@gmail.com>
2020-09-02 15:50:22 -0400

    marked implemented with CDN

--- fCC/Front End Libraries/Projects/markdown-previewer/src/App.js
+++ fCC/Front End Libraries/Projects/markdown-previewer/src/App.js
@@ -1,20 +1,30 @@
 import React, { Component } from "react";
 import "./App.css";
+import "./App.scss";
+import { Button } from "react-bootstrap";
+
+// import { Form } from "react-bootstrap";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: "",
-      output: "",
+      value:
+        "# Header ## Sub-header \n[fCC](https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer) \n`inline code` \n``` code block ``` \n1. list item \n> Blockquote \n[img]:https://d33wubrfki0l68.cloudfront.net/2f7693e1933ac514c960f51ceae72c91c6716eb2/b2efd/img/fcc_primary_small.svg \n**Bold text**",
+      markdowned: "not yet markdowned",
     };
   }
+
+  handleChange = (event) => {
+    const marked = window.marked;
+    this.setState({ value: event.target.value });
+    this.setState({ markdowned: marked(event.target.value) });
+    console.log(marked(event.target.value));
+  };
+
   render() {
     return (
       <div className="App">
-        <p>
-          Hello! <span variant="secondary">New</span>
-        </p>
         <div className="MarkdownMachine">
           {/* prettier-ignore */}
           <textarea
@@ -23,17 +33,14 @@ class App extends Component {
             id="editor"
             cols="50"
             rows="20"
+            value={this.state.value}
+            onChange={this.handleChange}
           >
-            # Header ## Sub-header \n
-            [fCC](https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer) \n
-            `inline code` \n
-            ``` code block ```` \n
-            1. list item
-            &gt; Blockquote
-            [img]:https://d33wubrfki0l68.cloudfront.net/2f7693e1933ac514c960f51ceae72c91c6716eb2/b2efd/img/fcc_primary_small.svg
-            **Bold text**
-          </textarea>
-          <p className="Preview">PREVIEW</p>
+            
+            </textarea>
+          <div id="preview" className="Preview">
+            {this.state.markdowned}
+          </div>
         </div>
       </div>
     );
---
+++ fCC/Front End Libraries/Projects/markdown-previewer/src/Content/Markdown placeholder.md
@@ -0,0 +1,19 @@
+# Header ## Sub-header
+
+[fCC](https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer)
+
+This is some `inline code`
+
+```js
+// And this is a
+{code block}
+```
+
+1. Now here's a list item...
+2. And another one.
+
+> Cool Blockquote
+
+[img]: https://d33wubrfki0l68.cloudfront.net/2f7693e1933ac514c960f51ceae72c91c6716eb2/b2efd/img/fcc_primary_small.svg
+
+**Bold text**
