ec388ee06b2dc6e66c509709d33514437ea23271
Victor Lazaro <lazarofilm@gmail.com>
2020-09-19 12:10:45 -0400

    first working version

--- fCC/Front End Libraries/Projects/markdown-previewer/src/App.js
+++ fCC/Front End Libraries/Projects/markdown-previewer/src/App.js
@@ -8,17 +8,52 @@ class App extends Component {
     super(props);
     this.state = {
       value:
-        "# Header ## Sub-header \n[fCC](https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer) \n`inline code` \n``` code block ``` \n1. list item \n> Blockquote \n[img]:https://d33wubrfki0l68.cloudfront.net/2f7693e1933ac514c960f51ceae72c91c6716eb2/b2efd/img/fcc_primary_small.svg \n**Bold text**",
-      markdowned:
-        '<h1 id="header--sub-header">Header ## Sub-header</h1> <p><a href="https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer">fCC</a> <code>inline code</code> <code>code block</code> </p> <ol> <li>list item <blockquote> <p>Blockquote </p> </blockquote> </li> </ol> <p><strong>Bold text</strong></p>',
+        "# Header\n## Sub-header\n[fCC](https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer)\n`inline code`\n``` code block ```\n1. list item\n> Blockquote\n[img]:https://d33wubrfki0l68.cloudfront.net/2f7693e1933ac514c960f51ceae72c91c6716eb2/b2efd/img/fcc_primary_small.svg\n**Bold text**",
+      markdowned: (
+        <div>
+          <h1 id="header">Header</h1>
+          <h2 id="sub-header">Sub-header</h2>
+          <p>
+            <a href="https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer">
+              fCC
+            </a>
+            <br />
+            <code>inline code</code>
+            <br />
+            <code>code block</code>
+          </p>
+          <ol>
+            <li>
+              list item
+              <blockquote>
+                <p>Blockquote </p>
+              </blockquote>
+            </li>
+          </ol>
+          <p>
+            <strong>Bold text</strong>
+          </p>
+        </div>
+      ),
     };
   }
 
   handleChange = (event) => {
     const marked = window.marked;
+    const mkdned = {
+      __html: window.marked(event.target.value, {
+        breaks: true,
+      }),
+    };
     this.setState({ value: event.target.value });
-    this.setState({ markdowned: marked(event.target.value) });
-    console.log(marked(event.target.value));
+    this.setState({
+      markdowned: <div dangerouslySetInnerHTML={mkdned} />,
+    });
+    console.log(
+      marked(event.target.value, {
+        breaks: true,
+      })
+    );
   };
 
   render() {
